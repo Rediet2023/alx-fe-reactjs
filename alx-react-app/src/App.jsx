fca32db22f2dc57644def3826d7a457f5ec62ef5
Rediet Derieb Hailemariam <redietd.hailemariam@gmail.com>
2024-08-11 12:11:44 +0200

    Add Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -37,6 +40,17 @@ function App() {
   return (
       <div>
           <WelcomeMessage />
+      </div>
+  );
+}
+
+export default App;
+function App() {
+  return (
+      <div>
+          <Header />
+          <MainContent />
+          <Footer />
       </div>
   );
 }
