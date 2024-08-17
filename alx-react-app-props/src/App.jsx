e1a010e87bb637f3d5bb1281c3c04c52d3bf6036
Rediet Derieb Hailemariam <redietd.hailemariam@gmail.com>
2024-08-17 18:17:52 +0200

    updated

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -6,7 +6,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import UserContext from '../UserContext';
+import UserContext from './UserContext';
 function App() {
   const [count, setCount] = useState(0)
 
