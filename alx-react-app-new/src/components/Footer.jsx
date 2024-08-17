c2f024aa4edaadbd8587318ce8d1259b3913aa0c
Rediet Derieb Hailemariam <redietd.hailemariam@gmail.com>
2024-08-17 17:31:25 +0200

    new directory added

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px', position: 'fixed', width: '100%', bottom: '0' }}>
+      <p>&copy; 2024 My Favorite Cities. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
+
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+function MainContent() {
+  return (
+      <main style={{ padding: '20px', backgroundColor: '#f0f0f0', color: '#333', lineHeight: '1.6' }}>
+          <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+  );
+}
+
+export default MainContent;
+
+
+
+ 
