491844d2c5aacd99490f9e1f3558259143dc69b5
Rediet <136799320+Rediet2023@users.noreply.github.com>
2024-09-01 17:15:05 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,36 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
-import ProfileDetails from './components/ProfileDetails';
-import ProfileSettings from './components/ProfileSettings';
-import Post from './components/Post';
-import Login from './components/Login';
-import ProtectedRoute from './ProtectedRoute';
-import { AuthProvider } from './AuthContext';
-
+import BlogPost from './components/BlogPost';
+import ProtectedRoute from './components/ProtectedRoute'; 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
+    <Router>
+      <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-
-          {/* Protect Profile route */}
-          <Route path="/profile/*" element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }>
-            <Route path="details" element={<ProfileDetails />} />
-            <Route path="settings" element={<ProfileSettings />} />
-          </Route>
-
-          {/* Dynamic route for blog posts */}
-          <Route path="/post/:postId" element={<Post />} />
+          {/* Protected Route for Profile */}
+          <Route path="/profile/*" element={<ProtectedRoute component={Profile} />} />
+          {/* Dynamic Route for Blog Post */}
+          <Route path="/blog/:id" element={<BlogPost />} />
         </Routes>
-      </Router>
-    </AuthProvider>
+      </div>
+    </Router>
   );
 }
 
