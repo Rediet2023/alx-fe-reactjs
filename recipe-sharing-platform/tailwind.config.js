f2789982e0a56ad87cb98ba7f9ea01e609b72d87
Rediet Derieb Hailemariam <redietd.hailemariam@gmail.com>
2024-09-15 18:14:36 +0200

    new update

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],  // Tailwind v2 purge config
+  darkMode: false,  // Or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 };
-
